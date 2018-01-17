4d13e00bdba9a440bd9029ddfe87da6e7e3166f8
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-17 15:52:34 +0700

    cleanup

--- src/authentication.js
+++ src/authentication.js
@@ -1,21 +1,21 @@
-const authentication = require('@feathersjs/authentication');
-const jwt = require('@feathersjs/authentication-jwt');
-const local = require('@feathersjs/authentication-local');
-const oauth2 = require('@feathersjs/authentication-oauth2');
-const FacebookStrategy = require('passport-facebook');
+const authentication = require('@feathersjs/authentication')
+const jwt = require('@feathersjs/authentication-jwt')
+const local = require('@feathersjs/authentication-local')
+const oauth2 = require('@feathersjs/authentication-oauth2')
+const FacebookStrategy = require('passport-facebook')
 
 module.exports = function (app) {
-  const config = app.get('authentication');
+  const config = app.get('authentication')
 
   // Set up authentication with the secret
-  app.configure(authentication(config));
-  app.configure(jwt());
-  app.configure(local());
+  app.configure(authentication(config))
+  app.configure(jwt())
+  app.configure(local())
 
   app.configure(oauth2(Object.assign({
     name: 'facebook',
-    Strategy: FacebookStrategy
-  }, config.facebook)));
+    Strategy: FacebookStrategy,
+  }, config.facebook)))
 
   // The `authentication` service is used to create a JWT.
   // The before `create` hook registers strategies that can be used
@@ -23,11 +23,11 @@ module.exports = function (app) {
   app.service('authentication').hooks({
     before: {
       create: [
-        authentication.hooks.authenticate(config.strategies)
+        authentication.hooks.authenticate(config.strategies),
       ],
       remove: [
-        authentication.hooks.authenticate('jwt')
-      ]
-    }
-  });
-};
+        authentication.hooks.authenticate('jwt'),
+      ],
+    },
+  })
+}
