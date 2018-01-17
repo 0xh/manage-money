397134123599686d1a6e1cf4d101ecd55ecb100a
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2018-01-17 16:20:46 +0700

    cleanup

--- src/models/users.model.js
+++ src/models/users.model.js
@@ -1,37 +1,37 @@
 // See http://docs.sequelizejs.com/en/latest/docs/models-definition/
 // for more of what you can do here.
-const Sequelize = require('sequelize');
-const DataTypes = Sequelize.DataTypes;
+const Sequelize = require('sequelize')
+const DataTypes = Sequelize.DataTypes
 
 module.exports = function (app) {
-  const sequelizeClient = app.get('sequelizeClient');
+  const sequelizeClient = app.get('sequelizeClient')
   const users = sequelizeClient.define('users', {
-  
+
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
-  
-  
+
+
     facebookId: { type: Sequelize.STRING },
-  
+
   }, {
     hooks: {
       beforeCount(options) {
-        options.raw = true;
-      }
-    }
-  });
+        options.raw = true
+      },
+    },
+  })
 
   users.associate = function (models) { // eslint-disable-line no-unused-vars
     // Define associations here
     // See http://docs.sequelizejs.com/en/latest/docs/associations/
-  };
+  }
 
-  return users;
-};
+  return users
+}
